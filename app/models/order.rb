bb650f0af4b639dd4c7b3caf21b2b2d7dac60418
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-10-12 19:43:54 -0400

    fix price issue

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ActiveRecord::Base
   belongs_to :user
   has_many :line_items
 
-  monetize :total_cents, numericality: true
+  monetize :price_cents, numericality: true
 
   validates :stripe_charge_id, presence: true
 

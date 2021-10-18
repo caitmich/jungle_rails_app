4a00e6b94ca2b633b029e0945f50ef637986bd4c
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-10-18 18:21:13 -0400

    add all tests for product module

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,17 +5,41 @@ RSpec.describe Product, type: :model do
 
     before do
       @category = Category.create(name:'Shoes')
+      @name = 'stilettos'
+      @price = 15000
     end
 
     describe '.create' do
       it 'saves successfully when all fields are completed' do
-        @product = Product.create(name: 'heels', quantity:50, price_cents:15000, category:@category)
-        # @product.save!
+        @product = Product.create(name: @name, quantity:50, price_cents:@price, category:@category)
         expect(@product.validate).to be true
       end
+
+      it 'does not save when name is not present' do
+        @product = Product.create(name: nil, quantity:50, price_cents: @price, category:@category)
+        expect(@product.validate).to be false
+        expect(@product.errors.full_messages).to eql(["Name can't be blank"])
+      end
+
+      it 'does not save when price is not present' do
+        @product = Product.create(name: @name, quantity:50, price_cents: nil, category:@category)
+        expect(@product.validate).to be false
+        expect(@product.errors.full_messages).to include("Price cents can't be blank")
+        expect(@product.errors.full_messages).to include("Price cents is not a number")
+      end
+
+      it 'does not save when quantity is not present' do
+        @product = Product.create(name: @name, price_cents: @price, quantity: nil, category:@category)
+        expect(@product.validate).to be false
+        expect(@product.errors.full_messages).to eq(["Quantity can't be blank"])
+      end
+
+      it 'does not save when category is not present' do
+        @product = Product.create(name: @name, quantity:50, price_cents: @price, category: nil)
+        expect(@product.validate).to be false
+        expect(@product.errors.full_messages).to eql(["Category can't be blank"])
+      end
+
     end
-
-
-
   end
 end

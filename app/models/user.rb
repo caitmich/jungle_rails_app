eaf6c8030aa7af176e8c8d72ea8ae6c94500807a
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-10-19 15:52:21 -0400

    implement user_spec tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: {case_sensitive: false}
 
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by_email(email)
+    user = User.find_by_email(email.downcase.strip)
 
     if user && user.authenticate(password)
       user
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
+  before do
+    @name = 'Joe'
+    @email = 'email@email.com'
+    @password = 'secretpassword'
+  end
+
   describe 'Validations' do 
-
-    before do
-      @name = 'Joe'
-      @email = 'email@email.com'
-      @password = 'secretpassword'
-    end
-
     describe '.create' do
 
       it 'does not create a user when name is not present' do
@@ -40,7 +39,7 @@ RSpec.describe User, type: :model do
         expect(@user2.errors.full_messages).to include("Email has already been taken")
       end
 
-      it 'does not create a user when email with capitals is the same as email without capitals in use' do
+      it 'does not create a user when email is repeated with different character cases' do
         @user = User.create!(name: @name, email:'email@email.com', password: @password, password_confirmation: @password)
         @user2 = User.new(name: @name, email: 'EMAIL@email.com', password: @password, password_confirmation: @password)
         @user2.save
@@ -54,6 +53,29 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to eq(["Password is too short (minimum is 8 characters)"])
       end
 
+    end
+
+  end
+
+  describe '.authenticate_with_credentials' do
+
+    it 'successfully logs in a user when authentications pass' do
+      @user = User.create!(name: @name, email:@email, password: @password, password_confirmation: @password)
+      expect(User.authenticate_with_credentials(@email, @password)).to eq(@user)
+    end
+
+    it 'still logs in a user if spaces are included in email field' do
+      @user = User.create!(name: @name, email:'email@email.com', password: @password, password_confirmation: @password)
+
+      expect(User.authenticate_with_credentials(' email@email.com ', @password)
+      ).to eq(@user)
+    end
+
+    it 'still logs in a user if case is changed in email field' do
+      @user = User.create!(name: @name, email:'email@email.com', password: @password, password_confirmation: @password)
+
+      expect(User.authenticate_with_credentials('EMAIL@emAil.coM', @password)
+      ).to eq(@user)
     end
 
   end
